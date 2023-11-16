a6c5bdfae8af1e562a1677b78281c949917dab78
Andarilz <isgrammar@gmail.com>
2023-11-16 19:43:17 +0300

    initial + ref

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+
+import { ref } from "vue"
+
+export default {
+  setup(){
+    const message = ref("Hello");
+    const num = ref(1)
+
+    const inc = () => num.value++
+
+    const dec = () => num.value--
+
+    return {
+      message,
+      num,
+      inc,
+      dec
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <h4>{{ num }}</h4>
+
+  <button @click="inc">+</button>
+  <br>
+  <button @click="dec">-</button>
+
+</template>
+
+<style>
+
+</style>
